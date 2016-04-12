ba7bc6d5889feaa8d2b6c1108f0ef196dc14df50
Hollis Lau <hollislau@hotmail.com>
2016-04-12 10:24:26 -0700

    Create a TCP server that logs incoming requests

---
+++ hollis_lau/lib/tcp_server.js
@@ -0,0 +1,13 @@
+const net = require("net");
+const fs = require("fs");
+
+const server = net.createServer((socket) => {
+  var file = new Date();
+  var writeOut = fs.createWriteStream(__dirname + "/../logs/" + file);
+
+  socket.pipe(writeOut);
+});
+
+server.listen(3000, () => {
+  process.stdout.write("Server listening on port 3000\n");
+});
